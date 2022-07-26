1028c1a00ac04be7a34123d7cf5d1c7006c9364c
LeeSeungHun <seung29954@gmail.com>
2022-07-26 17:39:32 +0900

    #3 게시글 목록 보기

--- src/main/java/com/cos/blog/controller/BoardController.java
+++ src/main/java/com/cos/blog/controller/BoardController.java
@@ -3,20 +3,25 @@ package com.cos.blog.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.cos.blog.config.auth.PrincipalDetail;
+import com.cos.blog.service.BoardService;
 
 @Controller
 public class BoardController {
 	
+	@Autowired
+	private BoardService boardService;
 
 	// http://localhost:8000/blog
+	// yml파일로 경로를 설정해서  /WEB-INF/views/index.jsp 으로 이동
 	@GetMapping("/")
-	public String index() {
-		
-		// yml파일로 경로를 설정해서  /WEB-INF/views/index.jsp 으로 이동
-		return "index";
+	public String index(Model model) {
+		model.addAttribute("boards", boardService.글목록());
+
+		return "index";  // viewResolver 작동 - index페이지로 model의 정보를 들고 이당한다.
 	}
 	
 	@GetMapping("/board/saveForm")
--- src/main/java/com/cos/blog/service/BoardService.java
+++ src/main/java/com/cos/blog/service/BoardService.java
@@ -2,6 +2,8 @@ package com.cos.blog.service;
 
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -27,4 +29,9 @@ public class BoardService {
 		board.setUser(user);
 		boardRepository.save(board);
 	}
+	
+	public List<Board> 글목록(){
+		return boardRepository.findAll();
+	}
+	
 }
